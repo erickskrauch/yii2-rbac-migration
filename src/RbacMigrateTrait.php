a9d3ca3bb6c183596b1adf673d3bba36153ff361
ilyaplot <ilyaplot@gmail.com>
2020-06-22 17:25:54 +0300

    Fixes #1 (#2)
    Fix `class_exists` call.

--- src/RbacMigrateTrait.php
+++ src/RbacMigrateTrait.php
@@ -1,7 +1,6 @@
 <?php
 namespace ErickSkrauch\Yii2;
 
-use yii\base\UnknownClassException;
 use yii\db\Query;
 use yii\helpers\Console;
 
@@ -260,11 +259,8 @@ trait RbacMigrateTrait
 
     private function ensureRuleClassExists($className, $name)
     {
-        try {
-            class_exists($className, true);
+        if (class_exists($className, true)) {
             return;
-        } catch (UnknownClassException $e) {
-            // fine, we are working on
         }
 
         $class = substr(strrchr($className, '\\'), 1);
